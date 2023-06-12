9a146fed66aee76e04a3b55e4ef30b76f7e7409c
Olena <olena@Olenas-MacBook-Pro.local>
2023-06-12 14:30:02 -0700

    Add route and controller function to retrieve all the users from the mongo DB.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const { 
-    createUser, 
+    createUser,
+    retrieveAllUsers,
 } = require('../../controllers/userController');
 
+// create new user
 router.post('/createUser', createUser);
+
+// retrieve all the users from the mongo DB
+router.get('/', retrieveAllUsers);
 
 module.exports = router;
