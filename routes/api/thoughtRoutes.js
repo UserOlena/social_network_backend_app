90925bc80d01123901e0a9e91977ae4ec9fc10b0
Olena <olena@Olenas-MacBook-Pro.local>
2023-06-13 18:02:50 -0700

    Implement route to retrieve a thought by ID.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {
     createThought,
     retrieveAllThoughts,
+    retrieveThoughtById,
 } = require('../../controllers/thoughtController');
 
 // add a new thought to the mongo DB
@@ -9,5 +10,8 @@ router.post('/createThought', createThought);
 
 // retrieve all the thoughts from the mongo DB
 router.get('/', retrieveAllThoughts);
+
+// retrieve all the thoughts from the mongo DB
+router.get('/:thoughtId', retrieveThoughtById);
 
 module.exports = router;
